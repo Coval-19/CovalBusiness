fce931a2a59c12792119af19b300e16a0a5fd9f8
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-11 19:37:03 +0300

    Added some components

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dashboard = (props) => {
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6">
+          Dashboard
+        </div>
+        <div className="col s12 m5 offset-m1">
+          Notifications
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
