3d854f243e529cff673d065750eb636c8321976d
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-11 19:37:43 +0300

    Added firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD_q9fHrUSydtO2kj_lzV9xUzRQZheqRgA",
+  authDomain: "coval-3c340.firebaseapp.com",
+  databaseURL: "https://coval-3c340.firebaseio.com",
+  projectId: "coval-3c340",
+  storageBucket: "coval-3c340.appspot.com",
+  messagingSenderId: "98186336113",
+  appId: "1:98186336113:web:404fdf9b8c2bfb5bce08d0",
+  measurementId: "G-HMX3NTQ002"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase
