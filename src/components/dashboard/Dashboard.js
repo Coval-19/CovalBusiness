e22f8183f3f858130adb22be19ebfa8a96771893
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-12 03:24:36 +0300

    Showing the QR in the dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import QR from './QR'
 
 const Dashboard = (props) => {
+  console.log(props)
+
   return (
-    <div className="dashboard container">
-      <div className="row">
-        <div className="col s12 m6">
-          Dashboard
-        </div>
-        <div className="col s12 m5 offset-m1">
-          Notifications
-        </div>
-      </div>
+    <div className="dashboard container white">
+      <h5 className="grey-text text-darken-3">Registeration Code</h5>
+      <p>
+        Please scan the QR code with your Coval Business App
+      </p>
+      <QR value={props.auth.uid} />
     </div>
   )
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+  return {
+    businesses: state.firestore.ordered.businesses,
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'businesses', orderBy: ['businessName', 'desc'] },
+  ])
+)(Dashboard)
