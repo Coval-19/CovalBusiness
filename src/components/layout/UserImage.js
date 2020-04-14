2b091ef9a37d4c3fe624310d69a6e5abd271758a
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-14 03:02:21 +0300

    Bug fix - tried to access firestore storage too many times

--- src/components/layout/UserImage.js
+++ src/components/layout/UserImage.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react'
+import { MediaBox } from 'react-materialize'
 import { getUserImagePromise } from '../../firebase/firebaseUtils'
 import EmptyAvatar from '../../assets/images/empty_avatar.jpg'
 
 class UserImage extends Component {
   state = {
-    url: EmptyAvatar
+    url: EmptyAvatar,
+    accessedFirestoreStorageOnce: false
   }
+
 
   render() {
     const {userId, userName} = this.props
 
-    getUserImagePromise(userId).then((url) => {
-      this.setState({
-        url: url
+    if (!this.state.accessedFirestoreStorageOnce) {
+      getUserImagePromise(userId).then((url) => {
+        this.setState({
+          url: url,
+          accessedFirestoreStorageOnce: true,
+        })
+
+      }).catch(error => {
+        console.log(error)
+        this.setState({
+          ...this.state,
+          accessedFirestoreStorageOnce: true,
+        })
       })
-    }).catch(error => {
-      console.log(error)
-    })
+    }
 
     return (
-      <img src={this.state.url} alt={userName} {...this.props} />
+      <img className="user-profile-image" src={this.state.url} alt={userName} {...this.props} />
+      // <MediaBox
+      //   options={{
+      //     inDuration: 275,
+      //     onCloseEnd: null,
+      //     onCloseStart: null,
+      //     onOpenEnd: null,
+      //     onOpenStart: null,
+      //     outDuration: 200
+      //   }}
+      // >
+      // </MediaBox>
     )
   }
 }
