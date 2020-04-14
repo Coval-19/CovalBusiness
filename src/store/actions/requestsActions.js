45c04408dfa073fe761a38df4aad877bbcb4b2fa
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-14 15:54:25 +0300

    Added actions to remove a notification (by id or userId)

--- src/store/actions/requestsActions.js
+++ src/store/actions/requestsActions.js
@@ -22,3 +22,52 @@ export const sendResponse = (userId, businessInfo, isApproved) => {
       })
   }
 }
+
+export const removeNotification = (businessId, notificationId) => {
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+    const firestore = firebase.firestore();
+
+    return firestore
+      .collection('businesses')
+      .doc(businessId)
+      .collection('notifications')
+      .doc(notificationId)
+      .delete()
+      .then(() => {
+        console.log('NOTIFICATION_DELETE_SUCCESS')
+        dispatch({ type: 'NOTIFICATION_DELETE_SUCCESS' })
+      }).catch(err => {
+        console.log('NOTIFICATION_DELETE_ERROR', err)
+        dispatch({ type: 'NOTIFICATION_DELETE_ERROR', err })
+      })
+  }
+}
+
+export const removeNotificationsByUser = (businessId, userId) => {
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+    const firestore = firebase.firestore();
+    const batch = firestore.batch();
+
+    return firestore
+      .collection('businesses')
+      .doc(businessId)
+      .collection('notifications')
+      .where('userId', '==', userId)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          batch.delete(doc.ref)
+        })
+        return batch.commit()
+      })
+      .then(() => {
+        console.log('NOTIFICATIONS_BY_USER_DELETE_SUCCESS')
+        dispatch({ type: 'NOTIFICATIONS_BY_USER_DELETE_SUCCESS' })
+      }).catch(err => {
+        console.log('NOTIFICATIONS_BY_USER_DELETE_ERROR', err)
+        dispatch({ type: 'NOTIFICATIONS_BY_USER_DELETE_ERROR', err })
+      })
+  }
+}
