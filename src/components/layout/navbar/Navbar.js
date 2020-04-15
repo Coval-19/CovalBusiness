b9f19df32f7ce4b701515788fe02074dfc4a0e82
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-15 19:31:01 +0300

    modified navbar

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import LoggedInLinks from './LoggedInLinks'
 import LoggedOutLinks from './LoggedOutLinks'
 import { connect } from 'react-redux'
+import ProfileImage from '../../profile/ProfileImage'
 
 class Navbar extends Component {
 
@@ -27,6 +28,18 @@ class Navbar extends Component {
     const { auth, profile } = this.props;
   
     const links = auth.uid ? <LoggedInLinks profile={profile} /> : <LoggedOutLinks />;
+    const userImageSidenav = auth.uid && (
+      <li>
+        <div className="user-view">
+          <div className="background" />
+          <Link to='/profile'>
+            <ProfileImage size="10em" />
+          </Link>
+          <a href="!#"><span className="white-text name">{this.props.profile.name}</span></a>
+          <a href="!#"><span className="white-text email">{this.props.auth.email}</span></a>
+        </div>
+      </li>
+    )
 
     return (
       <React.Fragment>
@@ -42,7 +55,8 @@ class Navbar extends Component {
           </div>
         </nav>
   
-        <ul className="sidenav" id="mobile-links">
+        <ul className="sidenav grey darken-3" id="mobile-links">
+          {userImageSidenav}
           {links}
         </ul>
       </React.Fragment>
